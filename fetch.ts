9cac4e8532d30e8042cc06d80850e6dd2aed2672
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-04-12 17:35:33 +0200

    add timeout on fetch, but a promise is never resolved...

--- fetch.ts
+++ fetch.ts
@@ -3,53 +3,59 @@
 const RE_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 const RE_TITLE = /<title>(.*)<\/title>/;
 
-async function fetchLinks({ url }) {
+type CrawledPage = {
+  title: String,
+  urls: Array<String>,
+};
+
+const fetchPage = ({ url }) => new Promise<CrawledPage>(async (resolve, reject) => {
+  const timeout = setTimeout(() => reject(new Error('timeout')), 2000);
   const res = await fetch(url);
+  clearTimeout(timeout);
   // console.log({ url, res });
-  const { contentType } = res.body;
-  // console.log({ url, contentType });
   const html = await res.text(); // we should await text() in all cases, otherwise a promise prevents the program to exit in the non-html case
-  if (contentType.includes('text/html')) {
-    const title = html.match(RE_TITLE).pop();
-    const urls = html.match(RE_URL) || [];
-    return {
-      title,
-      urls
-    };
+  if (res.body.contentType.includes('text/html')) {
+    resolve({
+      title: html.match(RE_TITLE).pop(),
+      urls: html.match(RE_URL) || []
+    });
+  } else {
+    reject(new Error('not an html page'));
   }
-}
+});
 
 async function crawl ({ remainingUrls = 1, url, addRecord }) {
   if (remainingUrls <= 0) return;
   console.log(`- fetching ${url}...`);
-  const page = await fetchLinks({ url });
-  if (!page) return; // not an html page
-  addRecord({ url, title: page.title });
-  for (let i in page.urls) {
-    await crawl({
-      remainingUrls: remainingUrls - Number(i) - 1,
-      url: page.urls[i],
-      addRecord
-    });
+  try {
+    const page = await fetchPage({ url });
+    addRecord({ url, title: page.title });
+    for (let i in page.urls) {
+      await crawl({
+        remainingUrls: remainingUrls - Number(i) - 1,
+        url: page.urls[i],
+        addRecord
+      });
+    }
+  } catch (err) {
+    console.error('crawl error:', err.message);
   }
 }
 
 (async () => {
-  const ALGOLIA_BLOG = 'https://blog.algolia.com/';
-  const NOT_HTML = 'https://blog.algolia.com/wp-content/uploads/2019/03/cropped-favicon-270x270.png';
-
-  const url = ALGOLIA_BLOG;
+  // const NOT_HTML = 'https://blog.algolia.com/wp-content/uploads/2019/03/cropped-favicon-270x270.png';
+  const url = 'https://blog.algolia.com/';
 
   // state management: list of crawled records
   const crawledRecords = []
   const addRecord = ({ url, title }) => crawledRecords.push({ url, title });
   
   // actual crawling process
-  const remainingUrls = 20;
+  const remainingUrls = 5;
   console.log(`crawling ${remainingUrls} pages from ${url}...`)
   await crawl({ remainingUrls, url, addRecord });
 
   // end of crawling
   console.log('crawled records:', JSON.stringify(crawledRecords, null, 2));
-  
+  console.log(`=> fetched records: ${crawledRecords.length}`);
 })();
