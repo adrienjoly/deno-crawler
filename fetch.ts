3e9a9ddac26617a9315a3a917e92845168908012
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-04-12 16:26:32 +0200

    use fetch()

--- fetch.ts
+++ fetch.ts
@@ -1,11 +1,6 @@
 // fetches a web page using HTTP client
 
-import {request} from "./../deno-request/request.ts"
-
 (async () => {
-  // warning: does not support HTTPS
-  const {status, headers, body} = await request("http://example.com");
-  const buf = new Deno.Buffer();
-  await Deno.copy(buf, body);
-  console.log(buf.toString());
+  const res = await fetch('https://blog.algolia.com');
+  console.log(await res.text());
 })();
